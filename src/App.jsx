dc6221260bbef6740cef6972cbb688c6bf06a85e
Denis Boldyrev <boldyrev.ds@gmail.com>
2018-01-14 20:00:37 +0300

    feat: add carrier select

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+import SelectFilter from './components/SelectFilter';
+
+const Container = styled.div`
+  width: 1170px;
+  padding-right: 15px;
+  padding-left: 15px;
+  margin-right: auto;
+  margin-left: auto;
+`;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Container>
+        <SelectFilter />
+      </Container>
     );
   }
 }
---
+++ src/components/SelectFilter/SelectFilter.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import Select from 'react-select';
+import 'react-select/dist/react-select.css';
+import { connect } from 'react-redux';
+import styled from 'styled-components';
+import { uniqBy } from 'lodash';
+import { changeSelection } from '../../AC/filters';
+
+const Wrapper = styled.div`
+  margin: 20px 0;
+`;
+
+const SelectFilter = (props) => {
+  const { flights, selected } = props;
+
+  const carriers = flights.map(flight => ({
+    label: flight.carrier,
+    value: flight.carrier,
+  }));
+
+  const handleChange = selectedCarriers =>
+    props.changeSelection(selectedCarriers.map(carrier => carrier.value));
+
+  return (
+    <Wrapper>
+      <Select
+        options={carriers}
+        value={selected}
+        placeholder="Type carrier name..."
+        multi
+        onChange={handleChange}
+      />
+    </Wrapper>
+  );
+};
+
+export default connect(
+  state => ({
+    selected: state.filters.selected,
+    flights: uniqBy(state.flights, 'carrier'),
+  }),
+  { changeSelection },
+)(SelectFilter);
